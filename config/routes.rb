40cd1c8a9b1366517083fb5ccc71cb7e01a2fa4a
garedean <garrettdolson@gmail.com>
2016-02-14 18:34:27 -0800

    Configure simple form, set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'dashboard/user'
+
   devise_for :users
+
+  # Set root to user login page
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
+  resources :users do
+    collection do
+      put 'reschedule'
+    end
+  end
+
+  resources :work_days
+
+  get '/dashboard', to: 'dashboard#user', as: 'dashboard'
 end
